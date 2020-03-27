fabab67dafc54c869ace792f662ce70c57cbcb02
Larysa_Shopuk <Larysa_Shopuk@epam.com>
2020-03-27 21:31:03 +0300

    Extend unit tests

--- ProductApi.Tests/ProductsServiceTests.cs
+++ ProductApi.Tests/ProductsServiceTests.cs
@@ -1,31 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using NUnit.Framework;
 using ProductAPI.Core;
 using ProductAPI.DAL.Interfaces;
 using ProductAPI.Services;
 using ProductAPI.Services.Interfaces;
 
-namespace ProductsApi.Tests
+namespace ProductApi.Tests
 {
-    [TestFixture]
+    [TestClass]
     public class ProductsServiceTests
     {
         private IProductService _productService;
         private Mock<IProductQuery> _productQueryMock;
         private Mock<IUnitOfWork> _unitOfWorkMock;
 
-        [SetUp]
+        [TestInitialize]
         public void Init()
         {
             _productQueryMock = new Mock<IProductQuery>(MockBehavior.Strict);
             _unitOfWorkMock = new Mock<IUnitOfWork>(MockBehavior.Strict);
             _productService = new ProductService(_productQueryMock.Object, _unitOfWorkMock.Object);
         }
-
-        [TestCaseSource(nameof(GetProduct_TestCases))]
+        
+        [TestMethod]
+        [DynamicData(nameof(GetProduct_TestCases), DynamicDataSourceType.Method)]
         public void GetProducts_Should_return_products_that_returned_by_query(IList<ProductDTO> expectedProducts)
         {
             _productQueryMock.Setup(x => x.GetAll()).Returns(expectedProducts);
@@ -39,16 +40,16 @@ namespace ProductsApi.Tests
             }
         }
 
-        private static IEnumerable<TestCaseData> GetProduct_TestCases()
+        private static IEnumerable<object[]> GetProduct_TestCases()
         {
             var product1 = new ProductDTO { Name = "Product1", ProductId = 1 };
             var product2 = new ProductDTO { Name = "Product2", ProductId = 2 };
             var product3 = new ProductDTO { Name = "Product3", ProductId = 3 };
 
-            yield return new TestCaseData(new List<ProductDTO>());
-            yield return new TestCaseData(new List<ProductDTO> { product1 });
-            yield return new TestCaseData(new List<ProductDTO> { product1, product2 });
-            yield return new TestCaseData(new List<ProductDTO> { product1, product2, product3 });
+            yield return new object[] { new List<ProductDTO>()};
+            yield return new object[] { new List<ProductDTO> { product1 }};
+            yield return new object[] { new List<ProductDTO> { product1, product2 }};
+            yield return new object[] {new List<ProductDTO> { product1, product2, product3 }};
         }
     }
 }
