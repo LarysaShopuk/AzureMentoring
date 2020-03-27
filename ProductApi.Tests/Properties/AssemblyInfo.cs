f4ef897a757619443a6529a300b3ae0deb7f3f00
Larysa_Shopuk <Larysa_Shopuk@epam.com>
2020-03-27 20:37:23 +0300

    Add new test project

---
+++ ProductApi.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProductApi.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("ProductApi.Tests")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fc624e32-d577-45ed-b27b-0ff0ecbf5115")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
